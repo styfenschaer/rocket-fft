49d402a1c80dab7aca0aaefbdc1d8fda353440d1
styfenschaer <79762922+styfenschaer@users.noreply.github.com>
2022-12-05 00:09:55 +0100

    add cache cleanup

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -4,14 +4,14 @@ from pathlib import Path
 import pytest
 
 
-# @pytest.fixture(autouse=True)
-# def numba_cache_cleanup():
-#     yield
-#     root = Path(__file__).parent
-#     cache_dir = root / '__pycache__'
-#     for file in os.listdir(cache_dir):
-#         path = cache_dir / file
-#         if path.suffix in ['.nbc', '.nbi']:
-#             os.remove(path)
+@pytest.fixture(autouse=True)
+def numba_cache_cleanup():
+    yield
+    root = Path(__file__).parent
+    cache_dir = root / '__pycache__'
+    for file in os.listdir(cache_dir):
+        path = cache_dir / file
+        if path.suffix in ['.nbc', '.nbi']:
+            os.remove(path)
 
 
