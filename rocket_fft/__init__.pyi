1b75dbb77338b5579dfc1d16dbe01ebc98234c51
styfenschaer <79762922+styfenschaer@users.noreply.github.com>
2023-03-12 13:02:45 +0100

    rephrase doc strings

--- rocket_fft/__init__.pyi
+++ rocket_fft/__init__.pyi
@@ -119,22 +119,30 @@ def good_size(target: int64, real: bool8) -> int64:
 def scipy_like() -> None:
     """Use SciPy-like type conversion. 
     - Conversion to real type:\\
-    float32 -> float32\\
-    complex64 -> float32\\
-    other -> float64  
+    `float32` -> `float32`\\
+    `complex64` -> `float32`\\
+    `Other` -> `float64`  
     - Conversion to complex type:\\
-    float32 -> complex64\\
-    complex64 -> complex64\\
-    other -> complex128  
+    `float32` -> `complex64`\\
+    `complex64` -> `complex64`\\
+    `Other` -> `complex128`  
+    
+    Handle the `axes` argument in the same way as SciPy. 
+    Passing duplicate axes to the `fft2`, `fftn`, `ifft2`, or `ifftn` 
+    functions will result in an error.
     """
 
 
 def numpy_like() -> None:
     """Use NumPy-like type conversion. 
     - Conversion to real type:\\
-    any -> float64  
+    `Any` -> `float64`  
     - Conversion to complex type:\\
-    any -> complex128  
+    `Any` -> `complex128`  
+    
+    Handle the `axes` argument in the same way as NumPy. 
+    Passing duplicate axes to the `fft2`, `fftn`, `ifft2`, or `ifftn` 
+    functions is allowed
     """
 
 
@@ -143,4 +151,6 @@ def get_workers() -> int:
 
 
 def set_workers(workers: int) -> None:
-    """Sets the default number of workers used."""
+    """Sets the default number of workers used.
+    This change cannot be undone after compilation of Rocket-FFT's internals.
+    """
