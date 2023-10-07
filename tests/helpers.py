9e708c5d5814332ac17b97aae750ba4424474e32
styfenschaer <79762922+styfenschaer@users.noreply.github.com>
2023-10-07 11:31:43 +0200

    update tests to use Numba 'new_style' error capture behavior

--- tests/helpers.py
+++ tests/helpers.py
@@ -4,13 +4,19 @@ from pathlib import Path
 import pytest
 
 
+def set_numba_capture_errors_new_style():
+    # See: https://numba.readthedocs.io/en/latest/reference/
+    # deprecation.html#deprecation-of-old-style-numba-captured-errors
+    os.environ["NUMBA_CAPTURED_ERRORS"] = "new_style"
+
+
 @pytest.fixture(autouse=True)
 def numba_cache_cleanup():
-    cache_dir = Path(__file__).parent / '__pycache__'
+    cache_dir = Path(__file__).parent / "__pycache__"
 
     for file in os.listdir(cache_dir):
         path = cache_dir / file
-        if path.suffix in ('.nbc', '.nbi'):
+        if path.suffix in (".nbc", ".nbi"):
             try:
                 os.remove(path)
             except (FileNotFoundError, PermissionError):
