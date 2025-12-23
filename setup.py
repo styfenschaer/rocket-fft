c18b44fdea28cd2aab03e493d9253a334a404afc
styfenschaer <79762922+styfenschaer@users.noreply.github.com>
2025-12-23 23:53:57 +0100

    Fix CI: move metadata to setup.py and remove setup.cfg

--- setup.py
+++ setup.py
@@ -2,9 +2,13 @@ import platform
 from pathlib import Path
 from tempfile import NamedTemporaryFile
 
-from setuptools import Extension, setup
+from setuptools import Extension, find_packages, setup
 from setuptools.command.build_ext import build_ext
 from setuptools.errors import CompileError
+
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 
 class build_ext_with_pthreads(build_ext):
@@ -45,8 +49,40 @@ if platform.system() == "Windows":
 else:
     extra_compile_args = ["-std=c++11", "-O3", "-Wall"]
 
-
 setup(
+    name="rocket-fft",
+    version="0.3.0",
+    description="Rocket-FFT extends Numba by scipy.fft and numpy.fft",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Styfen Schär",
+    author_email="styfen.schaer.blog@gmail.com",
+    url="https://github.com/styfenschaer/rocket-fft",
+    license="BSD-3-Clause",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Software Development",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+    ],
+    keywords=["FFT", "Fourier", "Numba", "SciPy", "NumPy"],
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={"rocket_fft": ["*.pyi"]},
+    python_requires=">=3.9",
+    install_requires=["numba>=0.60.0"],
+    extras_require={"dev": ["scipy>=1.13.1", "pytest>=8.4.2"]},
+    entry_points={"numba_extensions": ["init = rocket_fft:_init_extension"]},
     ext_modules=[
         Extension(
             "rocket_fft._pocketfft_numba",
